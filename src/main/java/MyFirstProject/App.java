e8811dea41d915c40376053b3d9b647f215d5c81
TmsC21 <t.cupela21@gmail.com>
2021-02-21 14:09:46 +0100

    Java commit

---
+++ src/main/java/MyFirstProject/App.java
@@ -0,0 +1,15 @@
+package MyFirstProject;
+
+import java.awt.*;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        System.out.println(age);
+    }
+}
